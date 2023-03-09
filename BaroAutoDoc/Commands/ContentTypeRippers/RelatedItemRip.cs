e7bf3f9218c5fb6c3ac0578a8b662970d2993e9a
Regalis11 <poe.regalis@gmail.com>
2023-03-09 15:57:57 +0200

    Added RelatedItemRip

---
+++ BaroAutoDoc/Commands/ContentTypeRippers/RelatedItemRip.cs
@@ -0,0 +1,29 @@
+﻿
+using BaroAutoDoc;
+using BaroAutoDoc.Commands;
+using BaroAutoDoc.SyntaxWalkers;
+
+sealed class RelatedItemRip : Command
+{
+    public void Invoke()
+    {
+        SyntaxRipperBuilder builder = new();
+
+        Directory.SetCurrentDirectory(GlobalConfig.RepoPath);
+
+        builder
+            .Prepare("RelatedItem")
+            .AddFile("Barotrauma/BarotraumaShared/SharedSource/Items/RelatedItem.cs")
+            .WithOptions(new ClassParsingOptions(new[] { "Load" }))
+            .Map
+            (
+                new FileMap("RelatedItem")
+                {
+                    "RelatedItem"
+                }
+            )
+            .Submit();
+
+        builder.Build(String.Empty);
+    }
+}
